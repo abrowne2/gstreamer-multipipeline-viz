2e80deb6ba39b60fac724db9bf6f765f8f656669
Adam Browne <adamo.browne@gmail.com>
2023-09-27 13:13:06 +0200

    PipelineViz: Initial websocket server

--- websocket_server/src/main.rs
+++ websocket_server/src/main.rs
@@ -1,42 +1,9 @@
-use tokio::net::TcpListener;
-use tokio_tungstenite::accept_async;
-use tokio_tungstenite::tungstenite::Message;
+pub mod socket_mgr;
+
+use crate::socket_mgr::MultipipelineVizServer;
 
 #[tokio::main]
 async fn main() {
-    let addr = "127.0.0.1:8080";
-    let listener = TcpListener::bind(addr).await.unwrap();
-    println!("Gstreamer multipipeline visualizer started at {}", addr);
-
-    loop {
-        let (stream, _) = listener.accept().await.unwrap();
-        tokio::spawn(async move {
-            let mut websocket = accept_async(stream).await.unwrap();
-            println!("WebSocket connection established");
-
-            while let Ok(msg) = websocket.read_message().await {
-                match msg {
-                    Message::Text(text) => {
-                        println!("Received message: {}", text);
-                        // Handle the received message here
-                    }
-                    Message::Binary(_) => {
-                        // Handle binary message if needed
-                    }
-                    Message::Ping(_) => {
-                        // Handle ping message if needed
-                    }
-                    Message::Pong(_) => {
-                        // Handle pong message if needed
-                    }
-                    Message::Close(_) => {
-                        // Handle close message if needed
-                        break;
-                    }
-                }
-            }
-
-            println!("WebSocket connection closed");
-        });
-    }
+    let server = MultiPipelineServer::new("127.0.0.1:8080".to_string());
+    server.start().await;
 }
--- websocket_server/src/socket_mgr/server.rs
+++ websocket_server/src/socket_mgr/server.rs
@@ -1,50 +1,112 @@
-use tokio::net::TcpListener;
-use tokio_tungstenite::accept_async;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::Mutex;
+use tokio_tungstenite::{accept_async, WebSocketStream};
 use tokio_tungstenite::tungstenite::Message;
+use futures::{StreamExt, SinkExt}; 
+use std::collections::HashMap;
+use std::sync::Arc;
+use rand::Rng;
+use serde_json::{from_str, Value};
 
-pub struct MultipipelineServer {
+pub struct MultipipelineVizServer {
     addr: String,
+    web_clients: Arc<Mutex<HashMap<String, Arc<Mutex<WebSocketStream<TcpStream>>>>>>, /* You can have more than one web client connected */
+    gstreamer_already_connected: Arc<Mutex<bool>>,
+    gstreamer_client_id: Arc<Mutex<Option<String>>>
 }
 
-impl MultipipelineServer {
-    fn new(addr: String) -> Self {
-        MultipipelineServer {
+impl MultipipelineVizServer {
+    fn new(addr: String, web_clients: Arc<Mutex<HashMap<String, Arc<Mutex<WebSocketStream<TcpStream>>>>>>) -> Self {
+        MultipipelineVizServer {
             addr,
+            web_clients,
+            gstreamer_already_connected: Arc::new(Mutex::new(false)),
+            gstreamer_client_id: Arc::new(Mutex::new(None))
         }
     }
 
-    async fn start(&self) {
+    // Generates a random client ID for the connected web client, if it does not exist (and is not gstreamer.)
+    pub async fn generate_client_id(&self) -> String {
+        let mut rng = rand::thread_rng();
+        let mut client_id = String::new();
+        let web_clients = self.web_clients.lock().await;
+        loop {
+            for _ in 0..10 {
+                let num = rng.gen_range(0..10);
+                client_id.push_str(&num.to_string());
+            }
+            if !web_clients.contains_key(&client_id) {
+                break;
+            }
+            client_id.clear();
+        }
+        client_id
+    }
+
+    async fn start(&mut self) {
         let listener = TcpListener::bind(&self.addr).await.unwrap();
         println!("Gstreamer multipipeline visualizer started at {}", self.addr);
-
+        let web_clients = Arc::clone(&self.web_clients);
+        
         loop {
-            let (stream, _) = listener.accept().await.unwrap();
-            tokio::spawn(async move {
-                let mut websocket = accept_async(stream).await.unwrap();
-                println!("WebSocket connection established");
+            let gstreamer_already_connected = Arc::clone(&self.gstreamer_already_connected);
+            let current_gstreamer_id = Arc::clone(&self.gstreamer_client_id);
 
-                while let Ok(msg) = websocket.read_message().await {
+            let (stream, _) = listener.accept().await.unwrap();
+            // We use the client identifier for sending to any tabs you have open.
+            let client_id_clone = self.generate_client_id().await;
+            let web_clients = Arc::clone(&web_clients);
+            
+            tokio::spawn(async move {
+                let websocket = accept_async(stream).await.unwrap();
+                let socket = Arc::new(Mutex::new(websocket));
+                web_clients.lock().await.insert(client_id_clone.clone(), Arc::clone(&socket));
+            
+                println!("WebSocket connection established");
+            
+                let mut webclient_socket = socket.lock().await;
+                while let Some(msg) = webclient_socket.next().await {
                     match msg {
-                        Message::Text(text) => {
-                            println!("Received message: {}", text);
-                            // Handle the received message here
+                        Ok(Message::Text(text)) => {
+                            match from_str::<Value>(&text) {
+                                Ok(json) => {
+                                    if json.get("is_gstreamer").map_or(false, |v| v.as_bool().unwrap_or(false)) {
+                                        println!("Gstreamer was connected, assigning it's client ID.");
+                                        *gstreamer_already_connected.lock().await = true;
+                                        *current_gstreamer_id.lock().await = Some(client_id_clone.clone());
+
+                                        // Gstreamer should not get it's own messages back.
+                                        web_clients.lock().await.remove(&client_id_clone);
+                                    }
+
+                                    // We are gstreamer, route all messages 
+                                    if client_id_clone == *current_gstreamer_id.lock().await.as_ref().unwrap_or(&String::new()) {
+                                        let mut clients = web_clients.lock().await;
+                                        for client in clients.values() {
+                                            
+                                            // how to build the trace model?
+                                            let mut client = client.lock().await;
+                                            client.send(Message::Text(text.clone())).await.unwrap();
+                                        }
+                                    }
+                                }
+                                Err(e) => {
+                                    println!("Error parsing JSON: {}", e);
+                                }
+                            }
                         }
-                        Message::Binary(_) => {
-                            // Handle binary message if needed
-                        }
-                        Message::Ping(_) => {
-                            // Handle ping message if needed
-                        }
-                        Message::Pong(_) => {
-                            // Handle pong message if needed
-                        }
-                        Message::Close(_) => {
-                            // Handle close message if needed
+                        Ok(Message::Close(_)) => {
+                            println!("Received socket close message");
                             break;
+                        }
+                        Ok(_) => {}
+                        Err(e) => {
+                            println!("Error reading message: {}", e);
                         }
                     }
                 }
 
+                web_clients.lock().await.remove(&client_id_clone);
                 println!("WebSocket connection closed");
             });
         }
