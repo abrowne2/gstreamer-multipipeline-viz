71e05f3f439ff5b8e2917d92afcbcfdde0aa1640
Adam Browne <adamo.browne@gmail.com>
2023-09-27 13:33:48 +0200

    Multipipeline VIz: Tracer will be built into gstreamer

--- websocket_server/src/README.md
+++
@@ -1,5 +0,0 @@
-# Multipipeline viz WebSocket server
-
-
-- Should receive chunks and pieces of the gstreamer traces of pipelines.
-- What should this data look like?
--- websocket_server/src/main.rs
+++
@@ -1,9 +0,0 @@
-pub mod socket_mgr;
-
-use crate::socket_mgr::MultipipelineVizServer;
-
-#[tokio::main]
-async fn main() {
-    let server = MultiPipelineServer::new("127.0.0.1:8080".to_string());
-    server.start().await;
-}
